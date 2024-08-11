e6fef342fe76fe194a4d44a9fbde90aec67fe24f
minhaj-ap <minhajap00@gmail.com>
2024-08-12 00:51:01 +0530

    refracted the code for better readablity

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-let steps2 = [];
+const bases = {
+  decimal: 10,
+  binary: 2,
+  octal: 8,
+  hexadecimal: 16,
+};
 var result;
+let fractionResults = null;
+var digits = [];
+let steps2 = [];
+let steps = [];
+let fractionSteps = [];
+var number;
+var fraction;
 function convert() {
-  var digits = [];
   result = null;
+  digits = [];
+  steps = [];
   steps2 = [];
-  let fractionSteps = [];
+  fractionSteps = [];
   var totalNumber = document.getElementById("numberInput").value;
   const firstOption = document.getElementById("firstOption").value;
   const secondOption = document.getElementById("secondOption").value;
   var resultSpace = document.getElementById("result");
   var stepsSpace = document.getElementById("steps");
-  stepsSpace.innerHTML = "";
   var steps2Space = document.getElementById("steps_2");
+  stepsSpace.innerHTML = "";
   steps2Space.innerHTML = "";
-  let fractionResults = null;
-  const bases = {
-    decimal: 10,
-    binary: 2,
-    octal: 8,
-    hexadecimal: 16,
-  };
   const first = bases[firstOption];
   const second = bases[secondOption];
-  let steps = [];
-  var number;
-  var fraction;
   if (totalNumber.includes(".")) {
     totalNumber = totalNumber.split(".");
     number = totalNumber[0];
@@ -38,158 +41,75 @@ function convert() {
   }
   if (fraction) {
     if (first == 10) {
-      FractionToDecimal();
+      FractionToDecimal(fraction, second);
     } else if (second == 10) {
-      FractionalDecimalTo();
+      FractionFromDecimal(fraction, first);
     }
-  }
-  if (first == 16) {
-    HexaDecimalConversion(number);
   }
   if (first == 10) {
-    ConversionFromDecimal();
-  } else if ((second == 10 || first == 8) && first != 16) {
-    ConvertToDecimal();
-  } else if (first == 2 && second == 8) {
-    BinaryToOctal();
+    ConvertFromDecimal(number, second);
+  } else if (second == 10) {
+    ConvertToDecimal(number, first);
   }
-  function ConversionFromDecimal() {
-    while (number >= second) {
-      var quotient = number % second;
-      steps.push([number, quotient]);
-      number = parseInt(number / second);
-      digits = [...digits, quotient];
-    }
-    if (number < second) {
-      steps.push([number]);
-      digits = [...digits, number];
-    }
-    digits = sortDigits(digits);
-    result = numberConversionToHex(digits);
-  }
-  function ConvertToDecimal() {
-    number = number.split("");
-    let numbers = sortDigits(number);
-    result = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      steps2.push(`${numbers[i]}x${first}^${i}=${numbers[i] * first ** i}`);
-      numbers[i] = numbers[i] * first ** i;
-      result += numbers[i];
-    }
-  }
-  function FractionToDecimal() {
-    fractionResults = ["."];
-    fraction = parseFloat(`0.${fraction}`);
-    let count = 0;
-    while (fraction != parseInt(fraction)) {
-      if (count === 6) {
-        break;
-      }
-      fractionSteps.push(
-        `${fraction}x${second}=${fraction * second} >>> ${parseInt(
-          fraction * second
-        )}`
-      );
-      fraction = fraction * second;
-      parts = fraction.toString().split(".");
-      fraction = parseFloat(`0.${parts[1]}`);
-      fractionResults = [...fractionResults, decimalToHex(parts[0])];
-      count++;
-    }
-    fractionResults = fractionResults.join("");
-  }
-  function FractionalDecimalTo() {
-    fractionResults = 0;
-    fraction = fraction.split("");
-    for (let i = 0; i < fraction.length; i++) {
-      fraction[i] = hexToDecimal(fraction[i]);
-      fractionResults += fraction[i] * first ** -(i + 1);
-      fractionSteps.push(
-        `${fraction[i]}x${first}^-${i + 1}=${fraction[i] * first ** -(i + 1)}`
-      );
-    }
-  }
-  steps.forEach((step) => {
-    var base = document.createElement("h3");
-    base.textContent = second;
-    var number = document.createElement("p");
-    number.textContent = step[0];
-    var balance = document.createElement("span");
-    balance.textContent = step[1];
-    var container = document.createElement("div");
-    container.appendChild(base);
-    container.appendChild(number);
-    container.appendChild(balance);
-    stepsSpace.appendChild(container);
-  });
-  steps2 = sortDigits(steps2);
-  steps2.forEach((step) => {
-    var text = document.createElement("h3");
-    text.textContent = step;
-    stepsSpace.appendChild(text);
-  });
-  fractionSteps.forEach((step) => {
-    var text = document.createElement("h3");
-    text.textContent = step;
-    steps2Space.appendChild(text);
-  });
+  htmlThings(stepsSpace, steps2Space, second);
   resultSpace.innerText = `Result:${result + fractionResults}`;
 }
-function numberConversionToHex(digits) {
-  const hexValues = {
-    0: 0,
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-    10: "A",
-    11: "B",
-    12: "C",
-    13: "D",
-    14: "E",
-    15: "F",
-  };
-  digits.forEach((digit) => {
-    digits[digits.indexOf(digit)] = hexValues[digit];
-  });
-  return digits.join("");
-}
-
-function HexaDecimalConversion(number) {
-  const hexNumberValues = {
-    0: 0,
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-    A: 10,
-    B: 11,
-    C: 12,
-    D: 13,
-    E: 14,
-    F: 15,
-  };
-  let digits = number.split("");
-  digits.forEach((digit) => {
-    digits[digits.indexOf(digit)] = hexNumberValues[digit];
-  });
-  digits = sortDigits(digits);
-  result = 0;
-  for (let i = 0; i < digits.length; i++) {
-    steps2.push(`${digits[i]}x16^${i}=${digits[i] * 16 ** i}`);
-    digits[i] = digits[i] * 16 ** i;
-    result += digits[i];
+function ConvertFromDecimal(number, second) {
+  while (number >= second) {
+    var quotient = number % second;
+    steps.push([number, quotient]);
+    number = parseInt(number / second);
+    digits = [...digits, decimalToHex(quotient)];
   }
+  if (number < second) {
+    steps.push([number]);
+    digits = [...digits, decimalToHex(number)];
+  }
+  digits = sortDigits(digits);
+  result = digits.join("");
+}
+function ConvertToDecimal(number, first) {
+  number = number.split("");
+  let numbers = sortDigits(number);
+  result = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = hexToDecimal(numbers[i]);
+    steps2.push(`${numbers[i]}x${first}^${i}=${numbers[i] * first ** i}`);
+    numbers[i] = numbers[i] * first ** i;
+    result += numbers[i];
+  }
+}
+function FractionFromDecimal(fraction, first) {
+  fractionResults = 0;
+  fraction = fraction.split("");
+  for (let i = 0; i < fraction.length; i++) {
+    fraction[i] = hexToDecimal(fraction[i]);
+    fractionResults += fraction[i] * first ** -(i + 1);
+    fractionSteps.push(
+      `${fraction[i]}x${first}^-${i + 1}=${fraction[i] * first ** -(i + 1)}`
+    );
+  }
+}
+function FractionToDecimal(fraction, second) {
+  fractionResults = ["."];
+  fraction = parseFloat(`0.${fraction}`);
+  let count = 0;
+  while (fraction != parseInt(fraction)) {
+    if (count === 6) {
+      break;
+    }
+    fractionSteps.push(
+      `${fraction}x${second}=${fraction * second} >>> ${parseInt(
+        fraction * second
+      )}`
+    );
+    fraction = fraction * second;
+    parts = fraction.toString().split(".");
+    fraction = parseFloat(`0.${parts[1]}`);
+    fractionResults = [...fractionResults, decimalToHex(parts[0])];
+    count++;
+  }
+  fractionResults = fractionResults.join("");
 }
 function sortDigits(digits) {
   let i = digits.length;
@@ -243,4 +163,30 @@ function hexToDecimal(number) {
     F: 15,
   };
   return hexValues[number];
+}
+function htmlThings(stepsSpace, steps2Space, second) {
+  steps.forEach((step) => {
+    var base = document.createElement("h3");
+    base.textContent = second;
+    var number = document.createElement("p");
+    number.textContent = step[0];
+    var balance = document.createElement("span");
+    balance.textContent = step[1];
+    var container = document.createElement("div");
+    container.appendChild(base);
+    container.appendChild(number);
+    container.appendChild(balance);
+    stepsSpace.appendChild(container);
+  });
+  steps2 = sortDigits(steps2);
+  steps2.forEach((step) => {
+    var text = document.createElement("h3");
+    text.textContent = step;
+    stepsSpace.appendChild(text);
+  });
+  fractionSteps.forEach((step) => {
+    var text = document.createElement("h3");
+    text.textContent = step;
+    steps2Space.appendChild(text);
+  });
 }
